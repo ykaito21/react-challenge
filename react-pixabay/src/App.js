99566ed5ca0f11e4ab941a4163c26e3a6e7350c8
Yuki Kaito <ykaito21@gmail.com>
2019-02-09 21:24:19 +0900

    practice with pixabay api

---
+++ react-pixabay/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Navbar from "./components/navbar/Navbar";
+import Search from "./components/search/Search";
+
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Navbar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ react-pixabay/src/components/navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppBar from "material-ui/AppBar";
+
+function Navbar() {
+  return <AppBar title="Pixabay" />;
+}
+
+export default Navbar;
