e53ce153bce0606518f0f8cdbb3277d1e199ebfe
Yuki Kaito <ykaito21@gmail.com>
2019-02-12 22:49:07 +0900

    practice with context part one

---
+++ react-lyric/src/components/layout/Index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tracks from "../tracks/Tracks";
+
+function Index() {
+  return (
+    <React.Fragment>
+      <Tracks />
+    </React.Fragment>
+  );
+}
+
+export default Index;
