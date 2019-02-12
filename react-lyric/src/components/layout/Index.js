d352a9b220ac3ec99ade5ff0ca111e69f088dfde
Yuki Kaito <ykaito21@gmail.com>
2019-02-12 23:54:14 +0900

    complete

--- react-lyric/src/components/layout/Index.js
+++ react-lyric/src/components/layout/Index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Tracks from "../tracks/Tracks";
+import Search from "../tracks/Search";
 
 function Index() {
   return (
     <React.Fragment>
+      <Search />
       <Tracks />
     </React.Fragment>
   );
